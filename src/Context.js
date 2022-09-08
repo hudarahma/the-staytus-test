94cd33aa6a8ce9d5c2d94144635845c923081bcd
Orkid <mrs.emadi@yahoo.com>
2022-09-08 09:55:33 +0400

    UPD:Context.js

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,13 @@
-import React, { createContext, useState, useEffect} from 'react';export const MyContext = createContext();
+import React, { createContext, useState, useEffect} from 'react';
+export const MyContext = createContext();
 
 const ContextProvider = ({ children }) => {
 
     const [ planets, setPlanets ] = useState('');
+    let  planetWithMovies  =  [];
+    const [ planetResidents, setPlanetResidents ] = useState([])
     const [ reptails, setReptails ] = useState([]);
+    const [finalPlanets, setFinalPlanets] = useState([]);
     
     const fetchData = async () => {
         const url = 'https://swapi.dev/api/';
@@ -16,24 +20,68 @@ const ContextProvider = ({ children }) => {
 
         const data = await result1.json();
         setPlanets(data.results);
-        console.log(data.results, 'data')
+        console.log(planets.length, 'planets');
+      
+       
+       for ( let i=0; planets.length > i; i++) {
+            console.log(planets[i].films.length)
+            if ( planets[i].films.length > 0 ) {
+                planetWithMovies.push( planets[i])
+            }
+            
+        }
+        // console.log(planetWithMovies, 'movies');
 
+        setPlanetResidents((planetWithMovies.filter((res, index) => res.residents.length > 0 ? res.residents[index] : 'no-residents')));
+        console.log(planetResidents,'res');
 
-        const result2 = await fetch(`${url}/species`, {
-            headers: {
-                Accept: 'application/json',
-            },
-        });
-        const data2 = await result2.json();
-        console.log(data2,'reptails');
-        setReptails(data2.results);
+        for ( let i = 0; i < planetResidents.length; i++) {
+            if (planetResidents[i].residents.length > 0 ) {
+                // console.log(planetResidents[i],'exist')
+                for ( let j = 0; j < planetResidents[i].residents.length; j++) {
+                //    console.log(planetResidents[i].residents[j])
+                   const getResident = await fetch(`${planetWithMovies[i].residents[j]}`,
+                        {
+                        headers: {
+                            Accept: 'application/json',
+                        },
+                    });
+                    const resident = await getResident.json();
+                   
+                    if (resident.species.length > 0) {
+                            let speciesFound = false;
+                            let x = 0;
+                            while (!speciesFound && x < resident.species.length) {
+                                const getSpecies = await fetch(`${resident.species[x]}`, {
+                                headers: {
+                                    Accept: 'application/json',
+                                },
+                                });
+                                const species = await getSpecies.json();
+                                // test Mammal
+                                if (species.classification === 'mammal') {
+                                    speciesFound = true;
+                                    finalPlanets.push(planetWithMovies[i]);
+                                }
+                                x++;
+                            }
+                        speciesFound = false;
+                    }   
+                }
+            }
+        }
+    
+        setFinalPlanets(finalPlanets);
+        console.log(finalPlanets, 'Mammal found');
     };
-
+    // console.log(resident.filter((res,index) => res.species.length > 0 ? res.species[index] : 'no-species'))
+    
 
     useEffect(() => {
         fetchData();
     }, []);
 
+    
 
     const initialState = {
         fetchData,
--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import formRedecer from './formSlice';
-
-export default configureStore({
-  reducer: {
-    form: formRedecer,
-  },
-});
